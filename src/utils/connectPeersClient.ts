2c2314fdc26d12996bf170fa6e38b030e41d9be7
progre <djyayutto@gmail.com>
2018-04-19 18:50:00 +0900

    fix: catch error in observable

--- src/utils/connectPeersClient.ts
+++ src/utils/connectPeersClient.ts
@@ -9,43 +9,47 @@ export default function connectPeersClient(
   opts: Peer.Options,
 ) {
   return new Observable<Peer.Instance>((subscribe) => {
-    const peer = new Peer(opts);
-    const signalHandler = (signal: any) => {
-      webSocket.send(JSON.stringify({
-        type: 'tunnel',
-        payload: { tunnelId, data: signal },
-      }));
-    };
-    peer.on('signal', signalHandler);
-    const messageHandler = (ev: MessageEvent) => {
-      const data: ConnectPeersServerMessage = JSON.parse(ev.data);
-      if (data.type !== 'tunnel' || data.payload.tunnelId !== tunnelId) {
-        return;
-      }
-      peer.signal(data.payload.data);
-    };
-    webSocket.addEventListener('message', messageHandler);
-    let removeAllListeners: Function;
-    const connectHandler = () => {
-      removeAllListeners();
-      webSocket.send(JSON.stringify({
-        type: 'tunnelComplete',
-        payload: { tunnelId },
-      }));
-      subscribe.next(peer);
-      subscribe.complete();
-    };
-    peer.once('connect', connectHandler);
-    const errorHandler = (err: Error) => {
-      removeAllListeners();
+    try {
+      const peer = new Peer(opts);
+      const signalHandler = (signal: any) => {
+        webSocket.send(JSON.stringify({
+          type: 'tunnel',
+          payload: { tunnelId, data: signal },
+        }));
+      };
+      peer.on('signal', signalHandler);
+      const messageHandler = (ev: MessageEvent) => {
+        const data: ConnectPeersServerMessage = JSON.parse(ev.data);
+        if (data.type !== 'tunnel' || data.payload.tunnelId !== tunnelId) {
+          return;
+        }
+        peer.signal(data.payload.data);
+      };
+      webSocket.addEventListener('message', messageHandler);
+      let removeAllListeners: Function;
+      const connectHandler = () => {
+        removeAllListeners();
+        webSocket.send(JSON.stringify({
+          type: 'tunnelComplete',
+          payload: { tunnelId },
+        }));
+        subscribe.next(peer);
+        subscribe.complete();
+      };
+      peer.once('connect', connectHandler);
+      const errorHandler = (err: Error) => {
+        removeAllListeners();
+        subscribe.error(err);
+      };
+      peer.once('error', errorHandler);
+      removeAllListeners = () => {
+        peer.removeListener('signal', signalHandler);
+        webSocket.removeEventListener('message', messageHandler);
+        peer.removeListener('connect', connectHandler);
+        peer.removeListener('error', errorHandler);
+      };
+    } catch (err) {
       subscribe.error(err);
-    };
-    peer.once('error', errorHandler);
-    removeAllListeners = () => {
-      peer.removeListener('signal', signalHandler);
-      webSocket.removeEventListener('message', messageHandler);
-      peer.removeListener('connect', connectHandler);
-      peer.removeListener('error', errorHandler);
-    };
+    }
   });
 }
