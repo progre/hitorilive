cf511df99461ec0af32a22656faa832d1c5d0b34
progre <djyayutto@gmail.com>
2018-04-24 19:13:09 +0900

    fix: fix server error on port changed
    #10

--- src/infrastructures/HTTPServer.ts
+++ src/infrastructures/HTTPServer.ts
@@ -1,3 +1,5 @@
+import debug from 'debug';
+const log = debug('hitorilive:HTTPServer');
 import http from 'http';
 import httpProxy from 'http-proxy';
 import net from 'net';
@@ -6,8 +8,9 @@ import WebSocket from 'ws';
 
 export default class HTTPServer {
   private server?: http.Server;
-  private proxy?: httpProxy = httpProxy.createProxyServer();
+  private proxy = httpProxy.createProxyServer();
   private readonly webSocketServer = new WebSocket.Server({ noServer: true });
+  private sockets = new Set<net.Socket>();
   port = 0;
 
   readonly onJoin = new Subject<WebSocket>();
@@ -23,10 +26,6 @@ export default class HTTPServer {
         this.handleAPIRequest(req, res);
         return;
       }
-      if (this.proxy == null) {
-        // already shutdowned
-        return;
-      }
       this.proxy.web(
         req,
         res,
@@ -35,14 +34,19 @@ export default class HTTPServer {
       );
     });
     this.server.on('upgrade', (req: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
+      if (this.server == null) {
+        // already shutdowned
+        socket.destroy();
+        return;
+      }
+      this.sockets.add(socket);
+      socket.once('close', () => {
+        this.sockets.delete(socket);
+      });
       if (req.url === '/join') {
         this.webSocketServer.handleUpgrade(req, socket, head, (ws) => {
           this.onJoin.next(ws);
         });
-        return;
-      }
-      if (this.proxy == null) {
-        // already shutdowned
         return;
       }
       this.proxy.ws(req, socket, head, { target: `ws://${mediaServerHost}` });
@@ -50,25 +54,25 @@ export default class HTTPServer {
     await new Promise((resolve, reject) => {
       this.server!.listen(port, resolve);
     });
+    log(`listen ${port}`);
     this.port = port;
   }
 
   async stopServer() {
-    const server = this.server!;
-    const proxy = this.proxy!;
+    if (this.server == null) {
+      return;
+    }
+    log('stopServer');
+    const server = this.server;
     this.server = undefined;
-    this.proxy = undefined;
-    await Promise.race([
-      Promise.all([
-        new Promise((resolve, reject) => {
-          server.close(resolve); // close isn't finish when client is connecting.
-        }),
-        new Promise((resolve, reject) => {
-          proxy.close(resolve);
-        }),
-      ]),
-      new Promise((resolve, reject) => { setTimeout(resolve, 3000); }),
-    ]);
+    // proxy isn't needed when no listen
+    this.sockets.forEach((socket) => {
+      socket.destroy();
+    });
+    await new Promise((resolve, reject) => {
+      server.close(resolve); // close isn't finish when client is connecting.
+    });
+    log('stopped');
   }
 
   private handleAPIRequest(
